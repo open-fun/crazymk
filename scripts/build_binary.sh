68edb7d94771ef25191bc9164be7519160e0e632
Stanislav Trifan <trifanstanislav@gmail.com>
2017-08-02 01:45:51 +0300

    Improved stages readability

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -2,4 +2,4 @@
 cd "${0%/*}"/..
 swipl --goal=main --stand_alone=true -o ./bin/crazymk -c ./src/main.pro \
     && cp -u ./scripts/run.sh ./bin/run.sh \
-    && echo "\nApplication build successfully...\n"
+    && echo "\nBuild passed\n"
